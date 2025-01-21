a1533d0815b18a336129aa1209d71f5d212320ea
marcossserafim <47904915+marcossserafim@users.noreply.github.com>
2025-01-21 00:21:08 -0300

    Add files via upload

---
+++ estruturasControle/for3.py
@@ -0,0 +1,14 @@
+produto = {'nome': 'Caneta Chic','preco': 14.90,
+           'importada': True, 'estoque': 793}
+
+for chave in produto:
+    print(chave)
+
+for valor in produto.values():
+    print(valor)
+    
+for chave, valor in produto.items():
+    print(chave, '=', valor)
+    
+print(chave, valor)
+    
